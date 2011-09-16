a9fa84b1b2666957b8998d02542e5891d0b88245
Nikolas Willingham <Queen of Argyll@.(none)>
2011-09-16 12:48:40 -0500

    ai-core.cpp: Added world info parameters to Update() ai-core.h: Added methods to return world and AI info ai-states.cpp: Added to AiChase::Execute() ai-test.cpp: Added a vector and dummy parameters to Warren.Update()

--- ai-core/ai-core/ai-test.cpp
+++ ai-core/ai-core/ai-test.cpp
@@ -2,10 +2,11 @@
 
 int main () 
 {
+	Vector3D vec;
 	AiManager Warren(1);
 	for (int i=0; i<20; ++i)
-	{ 
-			Warren.Update();
+	{
+			Warren.Update(1,2,3,4,vec,true);
 			Sleep(800);
 	}
 	return 0;
